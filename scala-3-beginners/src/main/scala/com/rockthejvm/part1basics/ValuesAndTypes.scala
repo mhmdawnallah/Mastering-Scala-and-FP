4e9086035820b566339cab3ab1a3433ca1ec9014
mhmdawnallah <mohamedmohey2352@gmail.com>
2023-04-12 16:12:48 +0200

    Learn about Scala Values and Types

---
+++ scala-3-beginners/src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,23 @@
+package com.rockthejvm.part1basics
+
+object ValuesAndTypes {
+
+  val grade = 98
+  val short: Short = 2312
+  val long: Long = 23142141421134L
+  val float: Float = 2312.4f
+  val isChecked: Boolean = false
+  val firstLetter: Char = 'c'
+  val name: String = "Mohamed Awnallah"
+
+
+  def main(args: Array[String]): Unit = {
+    println(grade)
+    println(short)
+    println(long)
+    println(float)
+    println(isChecked)
+    println(firstLetter)
+    println(name)
+  }
+}
